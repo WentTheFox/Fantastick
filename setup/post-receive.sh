9dbeb0ca2e09fa83c56e0d4b35d488cd25151442
WentTheFox <mail@went.tf>
2026-03-02 15:26:24 +0100

    Update post-receive hook with Prisma steps

--- setup/post-receive.sh
+++ setup/post-receive.sh
@@ -7,7 +7,9 @@ GIT="env -i git"
 CMD_PWD="cd .. && pwd"
 CMD_FETCH="$GIT fetch"
 CMD_NPM_INSTALL="npm ci"
+CMD_PRISMA_GENERATE="npx prisma generate"
 CMD_NPM_BUILD="npm run build"
+CMD_PRISMA_MIGRATE="npx prisma migrate deploy"
 CMD_RESTART="pm2 restart pm2.json"
 
 echo "$ $CMD_PWD"
@@ -22,8 +24,22 @@ else
   echo "# Skipping npm install, lockfile not modified"
 fi
 
+if $GIT diff --name-only $oldrev $newrev | grep "^prisma/schema.prisma"; then
+  echo "$ $CMD_PRISMA_GENERATE"
+  eval $CMD_PRISMA_GENERATE
+else
+  echo "# Skipping prisma generate, schema not modified"
+fi
+
 echo "$ $CMD_NPM_BUILD"
 eval $CMD_NPM_BUILD
+
+if $GIT diff --name-only $oldrev $newrev | grep "^prisma/migrations"; then
+  echo "$ $CMD_PRISMA_MIGRATE"
+  eval $CMD_PRISMA_MIGRATE
+else
+  echo "# Skipping prisma migrate, migrations not modified"
+fi
 
 echo "$ $CMD_RESTART"
 eval $CMD_RESTART

d957dce5a127c1eb81c7d7c14847d620c13cb6a4
WentTheFox <mail@went.tf>
2026-03-03 19:55:31 +0100

    Restore accidentally deleted emoji characters

--- src/constants/emoji-characters.ts
+++ src/constants/emoji-characters.ts
@@ -2,6 +2,8 @@ export enum EmojiCharacters {
   OCTAGONAL_SIGN = '🛑',
   WARNING_SIGN = '⚠️',
   GLOBE = '🌐',
+  WHITE_SQUARE = '⬜',
+  GREEN_SQUARE = '🟩',
   GREEN_CHECK = '✅',
   INFO = 'ℹ️',
   NO_ONE_UNDER_18 = '🔞',

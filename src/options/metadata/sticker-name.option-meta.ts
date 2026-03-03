f400449d8f2876baa2a122b43fe9828ed5ead225
WentTheFox <mail@went.tf>
2026-03-03 04:06:42 +0100

    Allow entire printable ASCII range for names

--- src/options/metadata/sticker-name.option-meta.ts
+++ src/options/metadata/sticker-name.option-meta.ts
@@ -1,7 +1,7 @@
 import { StringOptionMetadata } from '../../types/bot-interaction.js';
 import { ApplicationCommandOptionType } from 'discord-api-types/v10';
 
-export const stickerNameInvalidPattern = /[^\w -]/g;
+export const stickerNameInvalidPattern = /[^\x20-\x7E]/g;
 
 export const stickerNameOptionMeta = {
   type: ApplicationCommandOptionType.String,
